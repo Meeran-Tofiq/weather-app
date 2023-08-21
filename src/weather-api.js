278b07befd39b9fbb423a3f04fcf4e751e380d45
Meeran420 <101665499+Meeran420@users.noreply.github.com>
2023-08-21 15:55:13 +0300

    Reformat pages with prettier

--- src/weather-api.js
+++ src/weather-api.js
@@ -1,21 +1,21 @@
 const key = "0dad858d403c4228a32123248231908";
 
-const getWeatherUrl = function(city) {
-    return `https://api.weatherapi.com/v1/forecast.json?key=${key}&q=${city}&days=3`;
-}
+const getWeatherUrl = function (city) {
+  return `https://api.weatherapi.com/v1/forecast.json?key=${key}&q=${city}&days=3`;
+};
 
-const getWeatherInfo = function(city) {
-    fetch(getWeatherUrl(city))
-        .then((response) => {
-            return response.json();
-        })
-        .then(forecast => {
-            console.log(forecast)
-            return(forecast);
-        })
-        .catch(reason => {
-            console.error(reason);
-        })
-}
+const getWeatherInfo = function (city) {
+  fetch(getWeatherUrl(city))
+    .then((response) => {
+      return response.json();
+    })
+    .then((forecast) => {
+      console.log(forecast);
+      return forecast;
+    })
+    .catch((reason) => {
+      console.error(reason);
+    });
+};
 
-export { getWeatherInfo }
+export { getWeatherInfo };
