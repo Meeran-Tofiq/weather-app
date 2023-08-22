d73467ce834a64721972937a38fe8d9c76eb1cfa
Meeran420 <101665499+Meeran420@users.noreply.github.com>
2023-08-22 15:11:33 +0300

    Feat complex weather info of current with units

--- src/weather-input.js
+++ src/weather-input.js
@@ -3,18 +3,28 @@ import { getWeatherInfo } from './weather-api';
 const showForecast = async function (city = 'As Sulaymaniyah') {
     const allInfo = await getWeatherInfo(city);
 
+    const cel = document.querySelector('.temp-c').classList.contains('.active');
     console.log(allInfo);
-    fillCurrent(allInfo);
-    fillThreeDayForecast(allInfo.forecast);
+    fillCurrent(allInfo, cel);
+    fillThreeDayForecast(allInfo.forecast, cel);
 };
 
-const fillCurrent = function (info) {
+const fillCurrent = function (info, celsius) {
     const currentCont = document.querySelector('#current-weather');
     const basicCont = currentCont.querySelector('.current-basic');
     const complexCont = currentCont.querySelector('.current-complex');
     const city = info.location;
     const current = info.current;
 
+    const currentTemp = celsius ? current.temp_c : current.temp_f;
+    const tempUnit = celsius ? 'C' : 'F';
+    const currentFeelsLike = celsius
+        ? current.feelslike_c
+        : current.feelslike_f;
+    const currentWindSpeed = celsius ? current.wind_kph : current.mph;
+    const speedUnit = celsius ? 'kph' : 'mph';
+
+    // location and time
     currentCont.querySelector(
         '.location'
     ).textContent = `${city.name}, ${city.country}`;
@@ -23,10 +33,20 @@ const fillCurrent = function (info) {
         city.localtime.split(' ')[1]
     }`;
 
-    basicCont.querySelector('.temp').textContent = current.temp_c;
+    // basic info
+    basicCont.querySelector('.temp').textContent = `${currentTemp} ${tempUnit}`;
     basicCont.querySelector('.weather-desc').textContent =
         current.condition.text;
-    basicCont.querySelector('.feels-like').textContent = current.feelslike_c;
+    basicCont.querySelector(
+        '.feels-like'
+    ).textContent = `${currentFeelsLike} ${tempUnit}`;
+
+    //complex info
+    complexCont.querySelector(
+        '.wind-speed'
+    ).textContent = `${currentWindSpeed} ${speedUnit}`;
+    complexCont.querySelector('.humidity').textContent = `${current.humidity}%`;
+    complexCont.querySelector('.uv-index').textContent = `${current.uv}`;
 };
 
 const fillThreeDayForecast = function (info) {};
